54d8f6aab13a8255e12da00885cf6d6fbd87af21
filigreti <aolfiligre@gmail.com>
2024-06-28 11:41:59 +0100

    wip

--- src/components/currency.tsx
+++ src/components/currency.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { cn, convertCurrency } from '@/lib/utils'
+import { cn } from '@/lib/utils'
 import { currencyEnum, useCurrencyStore } from '@/store'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
@@ -8,19 +8,38 @@ import React, { useEffect, useState } from 'react'
 const Currency = ({ value, className }: { value: string, className?: string }) => {
     const { currency } = useCurrencyStore()
     const [convertedValue, setConvertedValue] = useState<string>(value)
+    const api = process.env.NEXT_PUBLIC_API_KEY
 
     useEffect(() => {
-        const updateCurrency = async () => {
+        const fetchExchangeRate = async () => {
             if (currency === currencyEnum.Naira) {
-                const result = await convertCurrency(value);
-                setConvertedValue(result);
+                try {
+                    const response = await axios.get(`https://v6.exchangerate-api.com/v6/${api}/pair/GBP/NGN`)
+                    const rate = response.data.conversion_rate
+                    const converted = parseFloat(value) * rate
+
+                    setConvertedValue(new Intl.NumberFormat('en-NG', {
+                        style: 'currency',
+                        currency: 'NGN'
+                    }).format(converted))
+
+                } catch (error) {
+                    console.error('Error fetching exchange rate:', error)
+                    setConvertedValue(new Intl.NumberFormat('en-GB', {
+                        style: 'currency',
+                        currency: 'GBP'
+                    }).format(parseFloat(value)))
+                }
             } else {
-                const result = await convertCurrency(value);
-                setConvertedValue(result);
+                setConvertedValue(new Intl.NumberFormat('en-GB', {
+                    style: 'currency',
+                    currency: 'GBP'
+                }).format(parseFloat(value)))
             }
         }
-        updateCurrency();
-    }, [currency, value])
+
+        fetchExchangeRate()
+    }, [currency, value, api])
 
     return (
         <div className={cn(`font-times font-light text-xl ${className}`)}>
